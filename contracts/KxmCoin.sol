224ba2f104eec89449664d807e797d43b702fb7e
lucong <332553882@qq.com>
2018-12-04 16:25:03 +0800

    ad

--- contracts/KxmCoin.sol
+++ contracts/KxmCoin.sol
@@ -5,10 +5,10 @@ contract KxmCoin is StandardToken {
   string public name = "KxmCoin";
   string public symbol = "KXC";  //token的代号 
   uint8 public decimals = 8;  //小数位
-  uint256 public INITIAL_SUPPLY = 10000000; //初始化代币总量，如这里1000万
+  uint256 public INITIAL_SUPPLY = 1000000000; //初始化代币总量，如这里1000万
   
   constructor() public{
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
+    totalSupply_ = INITIAL_SUPPLY * 10 ** uint256(decimals);
+    balances[msg.sender] = totalSupply_;
   }
 }
